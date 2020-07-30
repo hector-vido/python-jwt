498dd4dd57a3d23e3e6a745eed0c09056081278c
Hector Vido <hector_vido@yahoo.com.br>
2020-07-30 13:15:45 -0300

    Criando a autenticação

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import jwt
+from flask import Flask, request
+from ldap3 import Server, Connection
 
-encoded_jwt = jwt.encode({'some' : 'payload'}, 'secret', algorithm='HS256')
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/auth', methods=['POST'])
+def auth():
+  data = request.get_json()
+  print(data)
+  server = Server(environ['LDAP_HOST'], use_ssl=False)
+  ldap = Connection(server, user='uid={},ou=users,dc=example,dc=com'.format(data['usuario']), password=data['senha'])
+  if ldap.bind():
+    encode_jwt = jwt.encode({'some' : 'payload'}, 'secret', algorithm='HS256')
+    return redirect('/')
